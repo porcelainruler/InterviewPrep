f7b762cfa62450065111edbf56c06a9d9eee7b1f
Shubham <princeshubham22699@gmail.com>
2020-08-07 21:01:36 +0530

    DP Questions : Nth Stair with O-1 Space

--- PythonVSCode/DP_GFG/NStairReach.py
+++ PythonVSCode/DP_GFG/NStairReach.py
@@ -1,6 +1,47 @@
 #code
 from sys import stdin
 
+'''
+Java Code:
+// function to count ways to reach mth stair
+    Long countWays(int n){
+        
+        // your code here
+        if(n<=2)
+            return (long)n;
+    
+        long a=1;
+        long b=1;
+        long c=2;
+        for(int i=3 ; i<n+1 ; i++){
+            long temp = c;
+            c = (b+c)%1000000007;
+            a = b;
+            b = temp;
+        }
+    
+    return c%1000000007;
+        
+    }
+
+'''
+
+# Space- O(1)
+def dpSpace(n: int):
+    if n<=2:
+        return n
+    
+    a=1
+    b=1
+    c=2
+    for i in range(3, n+1):
+        temp = c
+        c = (b+c)%1000000007
+        a = b
+        b = temp
+    
+    return c
+
 def dp(n: int):
     if n <= 2:
         return n
@@ -22,7 +63,8 @@ def main():
         # arr = list(map(int, stdin.readline().split()))
         # sarr = stdin.readline()
         n = int(input())
-        ans = dp(n)
+        # ans = dp(n)
+        ans = dpSpace(n)
         print(ans)
 
 if __name__ == '__main__':
